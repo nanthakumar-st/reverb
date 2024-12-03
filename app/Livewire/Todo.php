4c4259760d55570e0bb5432318040d46f385da6c
nanthakumar-st <nanthakumar.m@arkinfotec.com>
2024-12-03 11:28:23 +0530

    Real time textbox value shown using reverb

--- app/Livewire/Todo.php
+++ app/Livewire/Todo.php
@@ -4,15 +4,30 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use Livewire\Attributes\Validate; 
+use Livewire\Attributes\On;
+
+use App\Events\TodoEvent;
 
 class Todo extends Component
 {
     #[Validate('required')] 
     public $todo = '';
 
+    public $todos = [];
+
+    #[On('echo:todo-channel,TodoEvent')]
+    public function todoList($todo)
+    {
+        $this->todos[] = $todo['todo'];
+    }
+
     public function submit()
     {
         $this->validate();
+
+        event(new TodoEvent($this->todo));
+
+        $this->todo = '';
     }
 
     public function render()
--- resources/views/livewire/todo.blade.php
+++ resources/views/livewire/todo.blade.php
@@ -1,6 +1,6 @@
-<div class="flex justify-center items-center h-full">
-    <form wire:submit.prevent="submit">
-        <div class="flex border w-full max-w-md">
+<div class="flex justify-center items-center h-full flex-col">
+    <form wire:submit.prevent="submit" class="w-full max-w-md">
+        <div class="flex border">
             <input type="text" wire:model="todo" placeholder="Enter text" class="flex-grow px-4 py-2" />
             <button type="submit" class="px-4 py-2 bg-blue-500 text-white hover:bg-blue-600">
                 Search
@@ -8,4 +8,9 @@
         </div>
         @error('todo') <span class="text-red-500 text-sm mt-2 block">{{ $message }}</span> @enderror 
     </form>
+    <ul class="w-full max-w-md">
+        @foreach ($todos as $val)
+            <li class="p-2 mb-1 border">{{ $val }}</li>
+        @endforeach
+    </ul>
 </div>
